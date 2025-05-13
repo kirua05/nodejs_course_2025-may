bddb08829e14dc28014daa286e848c3c8c3c83c0
Hsinyun <tsuua040506@gmail.com>
2025-05-13 18:18:46 +0800

    feat fix typo

--- express_demo/api.js
+++ express_demo/api.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 // 建立一個陣列來儲存使用者資料
-const todos = [];
+let todos = [];
 
 // 解析 JSON 格式
 app.use(express.json());
@@ -44,6 +44,10 @@ app.put('/todos/:id', (req, res) => {
   const { completed } = req.body
 
   const todo = todos.find((t) => t.id ===Number(id))
+  if (!todo) {
+    return res.status(404).json({ error: 'Todo not found' });
+  }
+
 
   todo.completed = completed
   res.json(todo)
@@ -51,7 +55,9 @@ app.put('/todos/:id', (req, res) => {
 })
 
 // DELETE
-app.delete('todos/:id', (req, res) => {
+app.delete('/todos/:id', (req, res) => {
+  console.log(req);
+  
   const { id } = req.params
 
   todos = todos.filter((t) => t.id !== Number(id))
